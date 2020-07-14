c9ed7bfad347eb232d86d9202b888d529badf7e6
lauren <lauren.hering@comcast.net>
2020-07-14 14:49:01 -0500

    finally

---
+++ GitHubPrac2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GitHubPrac2
+//
+//  Created by Lauren Hering on 7/14/20.
+//  Copyright © 2020 Lauren Hering. All rights reserved.
+//
+
+import UIKit
+
+// here is my stress comment
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //print("Hello world")
+
+
+}
+
